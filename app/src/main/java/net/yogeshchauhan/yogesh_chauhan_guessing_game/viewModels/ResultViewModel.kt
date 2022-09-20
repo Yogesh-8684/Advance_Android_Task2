9f5d95123e81a7872372fc586a3e2f3c84514b61
Yogesh-8684 <71631261+Yogesh-8684@users.noreply.github.com>
2022-09-20 16:07:56 -0400

    commit

---
+++ app/src/main/java/net/yogeshchauhan/yogesh_chauhan_guessing_game/viewModels/ResultViewModel.kt
@@ -0,0 +1,7 @@
+package net.yogeshchauhan.yogesh_chauhan_guessing_game.viewModels
+
+import androidx.lifecycle.ViewModel
+
+class ResultViewModel(result: String): ViewModel() {
+    val result = result
+}
